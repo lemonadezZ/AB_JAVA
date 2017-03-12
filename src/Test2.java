a4729d54efc33f3272d31c1a27e650227fe53141
jingzhixiang <jingzhixiang@aprbrother.com>
2017-03-12 12:02:29 +0800

    内部静态类

--- src/Test2.java
+++ src/Test2.java
@@ -1,8 +1,18 @@
+import java.util.Stack;
+
 /**
  * Created by jingz on 2017/3/12.
  */
 public class Test2 {
     String name="Test2";
+    static public class Sinner{
+        public void say(){
+            System.out.println("我是精通内部类的方法");
+        }
+        static public void says(){
+            System.out.println("我是精通内部类的静态方法");
+        }
+    }
     public class Inner {
         String name="Test2.Inner";
         public void say(String a){
@@ -15,13 +25,26 @@ public class Test2 {
         }
     }
     static public  void main(String []argv){
+        Test2.Sinner si1=new Test2.Sinner();
+        si1.say();
+        Test2.Sinner.says();
+
+
+
+        String name1="我是一个名字";
             class Inner {
+                    public String name="我是局部内部类";
                     public void say(String a){
                         System.out.println("我是内部类2");
                         System.out.println(a);
                     }
+                    public void show(){
+                        System.out.println(name1);
+                        System.out.println(this.name);
+                    }
             }
             Inner i=new Inner();
+            i.show();
             i.say("这是什么鬼东西");
             Test2 t2=new Test2();
             Test2.Inner i1=t2.new Inner();
