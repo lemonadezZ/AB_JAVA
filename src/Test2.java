6305f5188e40c6b1c60be5313481d25b0b140514
jingzhixiang <jingzhixiang@aprbrother.com>
2017-03-12 11:52:13 +0800

    测试内部类外部类相互调用

--- src/Test2.java
+++ src/Test2.java
@@ -2,10 +2,16 @@
  * Created by jingz on 2017/3/12.
  */
 public class Test2 {
+    String name="Test2";
     public class Inner {
+        String name="Test2.Inner";
         public void say(String a){
             System.out.println("我是内部类1");
             System.out.println(a);
+        }
+        public void  show(){
+            System.out.println(this.name);
+            System.out.println(Test2.this.name);
         }
     }
     static public  void main(String []argv){
@@ -20,5 +26,6 @@ public class Test2 {
             Test2 t2=new Test2();
             Test2.Inner i1=t2.new Inner();
             i1.say("这次应该对了");
+            i1.show();
     }
 }
